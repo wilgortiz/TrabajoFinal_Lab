ed7205b38aad1ee26adf2441435bd78c17868db9
PascualiDarius12 <dario12bifa@gmail.com>
2023-06-20 17:01:06 -0300

    agregar estado a clase ClienteData

--- src/ClasesData/ClienteData.java
+++ src/ClasesData/ClienteData.java
@@ -11,17 +11,17 @@ import java.util.List;
 import javax.swing.JOptionPane;
 
 public class ClienteData {
-    
+
     private Connection con = null;
-    
+
     public ClienteData() {
-        
+
         con = Conexion.getConexion();
-        
+
     }
-    
+
     public Cliente buscarCliente(int id) {
-        
+
         Cliente cliente = null;
         String sql = "SELECT  * FROM cliente WHERE idCliente=? ";
         PreparedStatement ps = null;
@@ -29,25 +29,26 @@ public class ClienteData {
             ps = con.prepareStatement(sql);
             ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
-            
+
             while (rs.next()) {
-                
+
                 cliente = new Cliente();
                 cliente.setIdCliente(rs.getInt("idCliente"));
                 cliente.setNombre(rs.getString("nombre"));
                 cliente.setApellido(rs.getString("apellido"));
                 cliente.setDomicilio(rs.getString("domicilio"));
                 cliente.setTelefono(rs.getString("telefono"));
-                
+                cliente.setEstado(rs.getBoolean("estado"));
+
             }
             ps.close();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Alumno " + ex.getMessage());
         }
-        
+
         return cliente;
     }
-    
+
     public Cliente buscarClientePorNombre(String nombre) {
         Cliente cliente = null;
         String sql = "SELECT * FROM cliente WHERE nombre=?";
@@ -56,7 +57,7 @@ public class ClienteData {
             ps = con.prepareStatement(sql);
             ps.setString(1, nombre);
             ResultSet rs = ps.executeQuery();
-            
+
             if (rs.next()) {
                 cliente = new Cliente();
                 cliente.setIdCliente(rs.getInt("idCliente"));
@@ -64,98 +65,104 @@ public class ClienteData {
                 cliente.setApellido(rs.getString("apellido"));
                 cliente.setDomicilio(rs.getString("domicilio"));
                 cliente.setTelefono(rs.getString("telefono"));
+                cliente.setEstado(rs.getBoolean("estado"));
             }
-            
+
             ps.close();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Cliente: " + ex.getMessage());
         }
-        
+
         return cliente;
     }
-    
+
     public void agregarCliente(Cliente cliente) {
-        String sql = "INSERT INTO cliente (apellido, nombre, domicilio, telefono) VALUES (?,?,?,?)";
+        String sql = "INSERT INTO cliente (apellido, nombre, domicilio, telefono, estado) VALUES (?,?,?,?,?)";
         PreparedStatement ps;
         try {
             ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            
+
             ps.setString(1, cliente.getApellido());
             ps.setString(2, cliente.getNombre());
             ps.setString(3, cliente.getDomicilio());
             ps.setString(4, cliente.getTelefono());
-            
+            ps.setBoolean(5, true);
+
             ps.executeUpdate();
-            
+
             ResultSet rs = ps.getGeneratedKeys();
-            
+
             if (rs.next()) {
                 cliente.setIdCliente(rs.getInt("idCliente"));
-                
+
                 JOptionPane.showMessageDialog(null, "Cliente agregado con exito");
             }
-            
+
             rs.close();
             ps.close();
-            
+
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Cliente" + ex.getMessage());
-            
+
         }
     }
 
     //VER
-    public void eliminarCliente(String tel) {
-        String sql = "DELETE FROM cliente WHERE telefono=?";
-        
-        PreparedStatement ps;
-        
-        try {
-            ps = con.prepareStatement(sql);
-            
-            ps.setString(1, tel);
-            
-            int aux = ps.executeUpdate();
-            if (aux == 1) {
-                JOptionPane.showMessageDialog(null, "Cliente eliminado con exito");
-            }
-            
-            ps.close();
-            
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Cliente" + ex.getMessage());
-        }
-    }
-    
-    public void modificarCliente(Cliente nuevoCliente, int idCliente) {
-        String sql = "UPDATE cliente SET apellido=? , nombre=? , domicilio=? , telefono=? WHERE idCliente=?";
-        
+    public void eliminarCliente(int id) {
+         String sql = "UPDATE cliente SET estado=? WHERE idCliente=?";
+
         try {
             PreparedStatement ps = con.prepareStatement(sql);
-            
+
+            ps.setBoolean(1, false);
+            ps.setInt(2, id);
+           
+
+            int exito = ps.executeUpdate();
+
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "El cliente se elimino correctamente");
+            } else {
+                JOptionPane.showMessageDialog(null, "Error al eliminar el cliente");
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla cliente" + ex.getMessage());
+        }
+    }
+
+    public void modificarCliente(Cliente nuevoCliente, int idCliente) {
+        String sql = "UPDATE cliente SET apellido=? , nombre=? , domicilio=? , telefono=?, estado=? WHERE idCliente=?";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+
             ps.setString(1, nuevoCliente.getApellido());
             ps.setString(2, nuevoCliente.getNombre());
             ps.setString(3, nuevoCliente.getDomicilio());
             ps.setString(4, nuevoCliente.getTelefono());
-            ps.setInt(5, idCliente);
-            
+            ps.setBoolean(5, nuevoCliente.isEstado());
+            ps.setInt(6, idCliente);
+
             int exito = ps.executeUpdate();
-            
+
             if (exito == 1) {
                 JOptionPane.showMessageDialog(null, "El cliente se actualizo correctamente");
             } else {
                 JOptionPane.showMessageDialog(null, "Error al actualizar el cliente");
             }
-            
+
             ps.close();
-            
+
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla cliente" + ex.getMessage());
         }
-        
+
     }
-    
-     public List<Cliente> listarClientesPorSubCadena(String subCadena) {
+
+    public List<Cliente> listarClientesPorSubCadena(String subCadena) {
 
         List<Cliente> clientes = new ArrayList<>();
 
@@ -163,7 +170,7 @@ public class ClienteData {
 
             String sql = "SELECT * FROM Cliente WHERE nombre LIKE ?";
             PreparedStatement ps = con.prepareStatement(sql);
-            ps.setString(1,  subCadena + "%");
+            ps.setString(1, subCadena + "%");
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 Cliente c = new Cliente();
@@ -173,7 +180,7 @@ public class ClienteData {
                 c.setApellido(rs.getString("apellido"));
                 c.setDomicilio(rs.getString("domicilio"));
                 c.setTelefono(rs.getString("telefono"));
-              
+                c.setEstado(rs.getBoolean("estado"));
 
                 clientes.add(c);
 
@@ -185,29 +192,32 @@ public class ClienteData {
         }
         return clientes;
     }
+
     public List<Cliente> listarClientes() {
-        
+
         List<Cliente> clientes = new ArrayList<>();
-        
+
         try {
-            
+
             String sql = "SELECT * FROM cliente";
             PreparedStatement ps = con.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 Cliente c = new Cliente();
-                
+
                 c.setIdCliente(rs.getInt("idCliente"));
                 c.setNombre(rs.getString("nombre"));
                 c.setApellido(rs.getString("apellido"));
                 c.setTelefono(rs.getString("telefono"));
                 c.setDomicilio(rs.getString("domicilio"));
+                c.setEstado(rs.getBoolean("estado"));
                 
+
                 clientes.add(c);
-                
+
             }
             ps.close();
-            
+
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, " Error al acceder a la tabla Clientes " + ex.getMessage());
         }
